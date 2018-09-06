c6a8e0b14a049517a5840ffec9ea878ddc47cb13
marci papineau <marcidenmark@hotmail.com>
2018-09-06 10:50:19 +0200

    commit

--- src/index.jsx
+++ src/index.jsx
@@ -22,22 +22,22 @@ import '../assets/stylesheets/application.scss';
 // state and reducers
 import carsReducer from './reducers/cars_reducer';
 
-// const initialState = {
-// 	// garage: {marci-garage},
-// 	cars: [
-// 		{ id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
-// 		{ id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
-// 		{ id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
-// 		{ id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
-// 	],
-// };
+const initialState = {
+	// garage: {marci-garage},
+	cars: [
+		{ id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
+		{ id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
+		{ id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
+		{ id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
+	],
+};
 // (state)
 // (this prompt comes immediately)
 const garageName = prompt("What is your garage's name?") || ` garage${Math.floor(10 + (Math.random() * 90))}`;
-const initialState = {
-	// garage: garageName,
-	cars:[]
-};
+// const initialState = {
+// 	// garage: garageName,
+// 	cars:[]
+// };
 
 //Reducers
 const reducers = combineReducers({
@@ -57,8 +57,8 @@ ReactDOM.render(
 
      		<Route path="/" exact component={CarsArray} />
 			<Route path ="/:garage/cars" component={App} />
-          	<Route path="/car/new" exact component={CarNew} />
-	          <Route path="/car/:id" component={CarShow} />
+{/*          	<Route path="/car/new" exact component={CarNew} />
+	          <Route path="/car/:id" component={CarShow} />*/}
 	</Switch>
 	</div>
 
